ca5561365da5b5d8ef9fdff221be6239e5f36050
moebamba01 <djigal001@gmail.com>
2024-08-31 12:01:01 +0000

    adding es6 ver to bubble

--- Sorts/bubble.js
+++ Sorts/bubble.js
@@ -16,4 +16,20 @@ function bubbleSort(arr) {
     return arr;
 }
 
-console.log(bubbleSort(arr)); // [2, 3, 4, 5, 8]
+// es6 version
+const bubbleSort = arr => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i -1; j++){
+            
+            if (arr[j] > arr[j +1]) {
+                let temp = arr[j];
+                arr[j] = arr[j +1];
+                arr[j +1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(arr)); // Output: [2, 3, 4, 5, 8, 10]
